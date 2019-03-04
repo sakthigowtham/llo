82990158cbaa6b8ba0408c963fcf4c1d96f164bf
sakthigowtham <42170124+sakthigowtham@users.noreply.github.com>
2019-03-04 14:16:50 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class sum
+{
+public  static void main(String arg[])
+{
+int w=0;
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+for(int i=1;i<=a;i++)
+{
+w+=i;
+System.out.println(w);
+}
+}}
